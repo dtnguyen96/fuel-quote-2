ccb6ae4cc93c0dbecfcf9222cdc04e365008558b
Dung Nguyen <dung34nguyen@gmail.com>
2021-02-10 20:58:02 -0600

    login and signup comp

---
+++ src/components/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+    return (
+        <div>
+            <h3>Login component</h3>
+        </div>
+    )
+}
+
+export default login
---
+++ src/components/signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signup = () => {
+    return (
+        <div>
+            <h3> Signup Component</h3>
+        </div>
+    )
+}
+
+export default signup
