1f3dd8fedf004d2a99cb88b1719c9ce4e526f390
Dung Nguyen <dung34nguyen@gmail.com>
2021-03-14 22:33:52 -0500

    add bcrypt, hash password, save user to db

--- src/backend/routers/userRouter.js
+++ src/backend/routers/userRouter.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/userModel")
+const bcrypt = require("bcryptjs");
+
 router.post("/", async (req, res) => {
     try {
         const { email, password, passwordVerify } = req.body;
@@ -23,6 +25,16 @@ router.post("/", async (req, res) => {
             return res
                 .status(400)
                 .json({ errorMessage: "An account exists with this email." });
+        
+        //hash the password 
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        //save a new user account to the db
+        const newUser = new User({
+            email, passwordHash
+        });
+        const savedUser = await newUser.save();
 
     } catch (err) {
         console.log(err);
