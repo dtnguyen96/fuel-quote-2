c693f006d74b5dd258ef9c869876b7f4afa47f94
Dung Nguyen <dung34nguyen@gmail.com>
2021-03-14 20:44:33 -0500

    finish User Model

--- src/backend/models/userModel.js
+++ src/backend/models/userModel.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    email: {type: String, required: true}
-})
+    email: {type: String, required: true, unique: true, trim: true},
+    passwordHash: {type: String, required: true},
+
+});
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
