8dc54a95904c69e617e8dce5c593056d8db88c21
Dung Nguyen <dung34nguyen@gmail.com>
2021-03-15 19:49:05 -0500

    trying to connect registering form to backend

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const cookieParser = require("cookie-parser");
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 app.use(express.json());
-
+app.use(cookieParser());
+app.use(cors({
+    origin: ["http://localhost:3000"]
+})
+);
 //uri from MongoDB Atlas dashboard
 const uri = process.env.ATLAS_URI;
 //Connect to the DB using mongoose
@@ -19,8 +25,6 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully")
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
 });
 app.use("/auth", require("./routers/userRouter"));
 
