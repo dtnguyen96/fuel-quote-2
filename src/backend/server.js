92636865821725c2a9d0e8eee25e10ff1963761b
Dung Nguyen <dung34nguyen@gmail.com>
2021-03-16 18:22:56 -0500

    Fixed cors policy errors

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,24 +8,23 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: ["http://localhost:3000"]
-})
-);
+app.use(cors());
 //uri from MongoDB Atlas dashboard
 const uri = process.env.ATLAS_URI;
 //Connect to the DB using mongoose
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully")
 })
 
-});
 
 app.use("/auth", require("./routers/userRouter"));
 app.use("/fuelform", require("./routers/formRouter"));
