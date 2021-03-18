dd1d22f7016db6fc73a1b74b9108b7d084a51349
Dung Nguyen <dung34nguyen@gmail.com>
2021-03-17 23:12:06 -0500

    finish auth + add loggedIn endpoint

--- src/backend/middleware/auth.js
+++ src/backend/middleware/auth.js
@@ -4,7 +4,10 @@ const auth = (req, res, next) => {
         if (!token) return res.status(401).json({errorMessage: "Unauthorized"});
 
         const verified = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(verified);
+        req.user = verified.user;
+
+        next();
+
     } catch (err){
         console.log(err);
         res.status(401).json({errorMessage: "Unauthorized"});
--- src/backend/routers/userRouter.js
+++ src/backend/routers/userRouter.js
@@ -101,4 +101,21 @@ router.get("/logout", (req, res) => {
     }).send();
     
 })
+
+//loggedIn
+
+router.get("/loggedIn", (req, res) => {
+        try {
+            const token = req.cookies.token;
+            if (!token) return res.status(401).json(false);
+    
+            jwt.verify(token, process.env.JWT_SECRET);
+            
+            res.send(true);
+    
+        } catch (err){
+            console.log(err);
+            res.json(false);
+        }
+});
 module.exports = router;
