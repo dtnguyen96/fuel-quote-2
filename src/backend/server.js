6565b56b141e668a0c88663a340a269a6d918bfe
Dung Nguyen <dung34nguyen@gmail.com>
2021-03-19 18:32:48 -0500

    fixed login

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//app.listen(port, () => console.log(`Server is running on port: ${port}`));
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -16,7 +16,7 @@ app.use(
         origin: ["http://localhost:3000"],
         credentials: true
     }));
-/*
+
 //uri from MongoDB Atlas dashboard
 const uri = process.env.ATLAS_URI;
 //Connect to the DB using mongoose
@@ -30,7 +30,7 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully")
 })
 
-*/
+
 app.use("/auth", require("./routers/userRouter"));
 app.use("/fuelform", require("./routers/formRouter"));
 module.exports = app;
