363ed01413336789753862e49dde7200b019b970
Neriyah Butler <46855482+neriyahbutler@users.noreply.github.com>
2021-04-02 23:48:09 -0500

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require("cookie-parser");
-const flash = require('connect-flash');
 
 require('dotenv').config();
 
