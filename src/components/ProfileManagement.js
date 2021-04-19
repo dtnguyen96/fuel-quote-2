0f1408d5ee7a7340ecd0681bbb83ce05093b09a5
neriyahbutler <neriyah.butler21@gmail.com>
2021-04-19 16:22:30 -0500

    Fixes issue with state fields not being saved in client (ProfileManagement)

--- src/components/ProfileManagement.js
+++ src/components/ProfileManagement.js
@@ -7,28 +7,32 @@ class ProfileManagement extends React.Component {
         super(props);
 
         this.state = {
-            fields : {}
+            fields : {
+                fullname: "",
+                address1: "",
+                address2: "",
+                city: "",
+                state: "",
+                zipcode: ""
+            }
         }
     }
 
-    handleChange(field, e)
+    handleChange = (field, e) => 
     {
         let fields = this.state.fields;
         
         fields[field] = e.target.value;
         this.setState({fields});
-        console.log("Changing state", field, "to", e.target.value)
     }
 
     handleValidation() {
-        console.log("Validating fields")
-        console.log(Object.keys(this.state.fields).length)
-
         Object.keys(this.state.fields).forEach(function(key) {
             try
-            {    console.log("Checking field")
+            {   
                 if (this.state.fields[key] == "")
-                    return false}
+                    return false
+            }
             catch {
                 console.log("error")
             }
@@ -36,10 +40,11 @@ class ProfileManagement extends React.Component {
         return true
     }
 
-    handleSubmission()
+    handleSubmission(e)
     {
-        let fields_json = this.state.fields
+        e.preventDefault();
 
+        let fields_json = this.state.fields
         if (this.handleValidation())
         {
             console.log("Validation passed!")
@@ -54,102 +59,101 @@ class ProfileManagement extends React.Component {
         }
         else {console.log("Validation failed")}
     }
-
     render(){
-    return (
-        <Form>
-            <Form.Group controlId = "fullname">
-                <Form.Label> Full Name </Form.Label>
-                <Form.Control type = "text" maxLength = {50} placeholder = "Full Name" onChange={this.handleChange.bind(this, "fullname")}/>
-            </Form.Group>
-
-            <Form.Group controlId = "address1">
-                <Form.Label> Address 1 </Form.Label>
-                <Form.Control type = "text" maxLength = {100} placeholder = "1234 Sesame Street" onChange={this.handleChange.bind(this, "address1")}/>
-            </Form.Group>
-
-            <Form.Group controlId = "address2">
-                <Form.Label> Address 2 </Form.Label>
-                <Form.Control type = "text" maxLength = {100} placeholder = "Apartment, Suite, Etc." onChange={this.handleChange.bind(this, "address2")}/>
-            </Form.Group>
-
-            <Form.Row>
-                <Form.Group as = {Col} controlId = "city">
-                    <Form.Label> City </Form.Label>
-                    <Form.Control type = "text" maxLength = {100} placeholder = "City" onChange={this.handleChange.bind(this, "city")}/>
+        return (
+            <Form>
+                <Form.Group controlId = "fullname">
+                    <Form.Label> Full Name </Form.Label>
+                    <Form.Control type = "text" maxLength = {50} placeholder = "Full Name" onChange={this.handleChange.bind(this, "fullname")}/>
                 </Form.Group>
 
-                <Form.Group as = {Col} controlId = "state">
-                    <Form.Label> State </Form.Label>
-                    <Form.Control as = "select" defaultValue = "" onChange={this.handleChange.bind(this, "state")}>
-                        <option> AL </option>
-                        <option> AK </option>
-                        <option> AZ </option>
-                        <option> AR </option>
-                        <option> CA </option>
-                        <option> CO </option>
-                        <option> CT </option>
-                        <option> DE </option>
-                        <option> FL </option>
-                        <option> GA </option>
-                        <option> HI </option>
-                        <option> ID </option>
-                        <option> IL </option>
-                        <option> IN </option>
-                        <option> IA </option>
-                        <option> KS </option>
-                        <option> KY </option>
-                        <option> LA </option>
-                        <option> ME </option>
-                        <option> MD </option>
-                        <option> MA </option>
-                        <option> MI </option>
-                        <option> MN </option>
-                        <option> MS </option>
-                        <option> MO </option>
-                        <option> MT </option>
-                        <option> NE </option>
-                        <option> NV </option>
-                        <option> NH </option>
-                        <option> NJ </option>
-                        <option> NM </option>
-                        <option> NY </option>
-                        <option> NC </option>
-                        <option> ND </option>
-                        <option> OH </option>
-                        <option> OK </option>
-                        <option> OR </option>
-                        <option> PA </option>
-                        <option> RI </option>
-                        <option> SC </option>
-                        <option> SD </option>
-                        <option> TN </option>
-                        <option> TX </option>
-                        <option> UT </option>
-                        <option> VT </option>
-                        <option> VA </option>
-                        <option> WA </option>
-                        <option> WV </option>
-                        <option> WI </option>
-                        <option> WY </option>
-                    </Form.Control>
+                <Form.Group controlId = "address1">
+                    <Form.Label> Address 1 </Form.Label>
+                    <Form.Control type = "text" maxLength = {100} placeholder = "1234 Sesame Street" onChange={this.handleChange.bind(this, "address1")}/>
                 </Form.Group>
-                
-                <Form.Group as = {Col} controlId = "zipcode">
-                    <Form.Label htmlFor = "zipcode"> Zip Code </Form.Label>
-                    <Form.Control maxLength = {9} id = "zipcode" placeholder = "Zip Code" aria-describedby = "zipcode" onChange={this.handleChange.bind(this, "zipcode")}/>
-                    <Form.Text id = "zipcode" muted>
-                        5 character code minimum required.
-                    </Form.Text>
-                </Form.Group>
-            </Form.Row>
 
-            <Button variant = "primary" type = "submit" onClick={this.handleSubmission()}>
-                Submit
-            </Button>
-        </Form>
-        )
-    }
+                <Form.Group controlId = "address2">
+                    <Form.Label> Address 2 </Form.Label>
+                    <Form.Control type = "text" maxLength = {100} placeholder = "Apartment, Suite, Etc." onChange={this.handleChange.bind(this, "address2")}/>
+                </Form.Group>
+
+                <Form.Row>
+                    <Form.Group as = {Col} controlId = "city">
+                        <Form.Label> City </Form.Label>
+                        <Form.Control type = "text" maxLength = {100} placeholder = "City" onChange={this.handleChange.bind(this, "city")}/>
+                    </Form.Group>
+
+                    <Form.Group as = {Col} controlId = "state">
+                        <Form.Label> State </Form.Label>
+                        <Form.Control as = "select" defaultValue = "" onChange={this.handleChange.bind(this, "state")}>
+                            <option> AL </option>
+                            <option> AK </option>
+                            <option> AZ </option>
+                            <option> AR </option>
+                            <option> CA </option>
+                            <option> CO </option>
+                            <option> CT </option>
+                            <option> DE </option>
+                            <option> FL </option>
+                            <option> GA </option>
+                            <option> HI </option>
+                            <option> ID </option>
+                            <option> IL </option>
+                            <option> IN </option>
+                            <option> IA </option>
+                            <option> KS </option>
+                            <option> KY </option>
+                            <option> LA </option>
+                            <option> ME </option>
+                            <option> MD </option>
+                            <option> MA </option>
+                            <option> MI </option>
+                            <option> MN </option>
+                            <option> MS </option>
+                            <option> MO </option>
+                            <option> MT </option>
+                            <option> NE </option>
+                            <option> NV </option>
+                            <option> NH </option>
+                            <option> NJ </option>
+                            <option> NM </option>
+                            <option> NY </option>
+                            <option> NC </option>
+                            <option> ND </option>
+                            <option> OH </option>
+                            <option> OK </option>
+                            <option> OR </option>
+                            <option> PA </option>
+                            <option> RI </option>
+                            <option> SC </option>
+                            <option> SD </option>
+                            <option> TN </option>
+                            <option> TX </option>
+                            <option> UT </option>
+                            <option> VT </option>
+                            <option> VA </option>
+                            <option> WA </option>
+                            <option> WV </option>
+                            <option> WI </option>
+                            <option> WY </option>
+                        </Form.Control>
+                    </Form.Group>
+                    
+                    <Form.Group as = {Col} controlId = "zipcode">
+                        <Form.Label htmlFor = "zipcode"> Zip Code </Form.Label>
+                        <Form.Control maxLength = {9} id = "zipcode" placeholder = "Zip Code" aria-describedby = "zipcode" onChange={this.handleChange.bind(this, "zipcode")}/>
+                        <Form.Text id = "zipcode" muted>
+                            5 character code minimum required.
+                        </Form.Text>
+                    </Form.Group>
+                </Form.Row>
+
+                <Button variant = "primary" type = "submit" onClick={this.handleSubmission.bind(this)}>
+                    Submit
+                </Button>
+            </Form>
+            )
+        }
 }
 
 export default ProfileManagement
