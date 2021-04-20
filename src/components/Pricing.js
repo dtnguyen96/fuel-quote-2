7935e71a564029ba0b81fc98190ee4efbb80f083
Dung Nguyen <dung34nguyen@gmail.com>
2021-04-19 19:02:28 -0500

    pricing module logic

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,21 +1,32 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+customerOne = {location: "TX", history: true, gallon: 1500}
+customerTwo = {location: "CA", history: false, gallon: 12}
 
-class Pricing extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            fields : {},
-            errors: {}
-        }
+function calcPricePerGallon(customer){
+    let locationFactor = 0;
+    let historyFactor = 0;
+    let gallonFactor = 0;
+    const profitFactor = 0.1;
+    let margin = 0;
+    if (customer.location === "TX"){
+        locationFactor = 0.02
     }
-
-    render() {
-        return (
-            <div>
-                Pricing module goes here
-            </div>
-        )
+    else {
+        locationFactor = 0.04
     }
+    if (customer.history){
+        historyFactor = 0.01
+    }
+    if (customer.gallon > 1000){
+        gallonFactor = 0.02
+    }
+    else {
+        gallonFactor = 0.03
+    }
+    margin = ( locationFactor - historyFactor + gallonFactor + profitFactor) * 1.50
+    console.log(margin)
+    return 1.5 + margin
 }
+function calcTotal (customerIn) {
+    return calcPricePerGallon(customerIn) * customerIn.gallon
+}
+console.log(calcTotal(customerOne));
